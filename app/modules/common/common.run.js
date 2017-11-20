c8972a4cb8dde526534059d358c9b47482d12a1d
Alyson Maia <alyson.maia@gmail.com>
2017-11-19 23:08:45 -0300

    App load time counter added

--- app/modules/common/common.run.js
+++ app/modules/common/common.run.js
@@ -1,9 +1,15 @@
 (function() {
     'use strict';
 
-    run.$inject = [];
-    function run() {
-        console.log((new Date) - a + "ms");
+    run.$inject = ['$transitions'];
+    function run($transitions) {
+        $transitions.onSuccess({ }, _onSuccess);
+    }
+
+    function _onSuccess(transition) {
+        if (transition.from().name === '') {
+            console.log("App load time: "+ ((new Date)-initTime)/1000 +"s");
+        }
     }
 
     angular.module('petriNet.common').run(run);
