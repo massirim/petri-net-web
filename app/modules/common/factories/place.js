b2614df4ad6f6f91f20ab0ae981501445fdfe0b0
Alyson Maia <alyson.maia@gmail.com>
2018-03-11 21:14:52 -0300

    New PlaceFactory structure

--- app/modules/common/factories/place.js
+++ app/modules/common/factories/place.js
@@ -3,231 +3,257 @@
 
     /** TODO
      * @ngdoc service
-     * @name petriNet.common.factory:places
+     * @name petriNet.common.factory:placeFactory
      * @description
      * description...
      **/
-    places.$inject = ['configFactory', 'svgAssetsFactory'];
-    function places(configFactory, svgAssetsFactory) {
+    placeFactory.$inject = ['configFactory', 'svgAssetsFactory'];
+
+    function placeFactory(configFactory, svgAssetsFactory) {
         var factory = {
-            newPlace: newPlace,
-            setTokens: setTokens
+            newPlace: newPlace
         };
 
         return factory;
 
-        ///// Functions /////
-        /** TODO
-         * @ngdoc method
-         * @name newPlace
-         * @methodOf petriNet.common.factory:places
-         * @param {type} param description...
-         * @returns {type} description...
-         * @description
-         * description...
-         **/
         function newPlace(container, x, y, label, tokens) {
-            var diameter = configFactory.get().nodeSize.place.diameter;
-            var style = configFactory.get().nodeStyle.place;
-            var hasShadow = configFactory.get().nodeStyle.place.shadow;
+            var place = _drawPlace();
+            // Methods
+            place.getTokens = getTokens;
+            place.setTokens = setTokens;
+            // Private
+            var _tokens = tokens;
 
-            var placeElement = container
-                .group()
-                .draggy()
-                .circle(diameter)
-                .move(x, y)
-                .attr(style)
-                .addClass('place');
+            _init();
 
-            setTokens(placeElement, tokens);
-            svgAssetsFactory.addLabel(placeElement, label);
-            if (hasShadow)
-                svgAssetsFactory.addDropShadow(placeElement);
+            return place;
 
-            return placeElement;
-        }
-
-        /** TODO
-         * @ngdoc method
-         * @name methodName
-         * @methodOf petriNet.common.factory:svgAssetsFactory
-         * @param {type} param description...
-         * @returns {type} description...
-         * @description
-         * Set the visual of the place to the specified number of tokens
-         **/
-        function setTokens(place, qty) {
-            var tokensCount = _countTokens(place);
-
-            // Stops for invalid quantity values
-            if(qty < 0 || tokensCount === qty || typeof qty !== 'number') return;
-
-            // Less than 4 means dot tokens should be drawn
-            // otherwise number tokens
-            if ( qty <= 4 ) {
-                if ( tokensCount > 4 ) {
-                    // Remove text token inside
-                    _getTokens(place).remove();
-                } else if (tokensCount > qty) {
-                    // Dot Tokens should be removed
-                    // _drawDotTokens will only adust position
-                    var tokensToRemoved = tokensCount - qty;
-                    var tokensList = _getTokens(place);
-                    while(tokensToRemoved-- > 0) {
-                        tokensList[tokensToRemoved].remove();
-                    }
-                }
-                // Draw dot token
-                _drawDotTokens(place, qty);
-            } else {
-                if ( tokensCount > 0 && tokensCount <= 4 ) {
-                    // Remove dot tokens inside
-                    _getTokens(place)
-                    .forEach(function (token) {
-                    token.remove();
-                    });
-                } else if (tokensCount >= 5) {
-                    // Remove text token inside
-                    _getTokens(place).remove();
-                }
-                _drawNumberToken(place, qty);
+            ///// Functions /////
+            function _init() {
+                place.setTokens(_tokens);
+                svgAssetsFactory.addLabel(place, label);
+                var hasShadow = configFactory.get().nodeStyle.place.shadow;
+                if (hasShadow)
+                    svgAssetsFactory.addDropShadow(place);
             }
-        }
 
-        function _drawDotTokens(place, qty) {
-            var tokensCount = _countTokens(place);
+            /**
+             * Creates a new place element on the svg and returns it
+             **/
+            function _drawPlace() {
+                var diameter = configFactory.get().nodeSize.place.diameter;
+                var style = configFactory.get().nodeStyle.place;
 
-            // Draw more dots if needed
-            for (var i = qty - tokensCount; i > 0; i--) {
+                var placeElement = container
+                    .group()
+                    .draggy()
+                    .circle(diameter)
+                    .move(x, y)
+                    .attr(style)
+                    .addClass('place');
+
+                return placeElement;
+            }
+
+            /**
+             * Returns the quantity of tokens in the actual place
+             **/
+            function getTokens() {
+                return _tokens;
+            }
+
+            /**
+             * Returns the quantity of tokens in the actual place
+             **/
+            function setTokens(quantity) {
+                var tokensCount = _countTokenElements();
+                // Set the new tokens quantity
+                _tokens = quantity;
+
+                // Stops for invalid quantity values
+                if (quantity < 0 || tokensCount === quantity || typeof quantity !== 'number') return;
+
+                // Less than 4 means dot tokens should be drawn
+                // otherwise number tokens
+                if (quantity <= 4) {
+                    if (tokensCount > 4) {
+                        // Remove text token inside
+                        _getTokensElements().remove();
+                    } else if (tokensCount > quantity) {
+                        // Dot Tokens should be removed
+                        // _drawDotTokens will only adust position
+                        var tokensToRemoved = tokensCount - quantity;
+                        var tokensList = _getTokensElements();
+                        while (tokensToRemoved-- > 0) {
+                            tokensList[tokensToRemoved].remove();
+                        }
+                    }
+                    // Draw dot token
+                    _drawDotTokens(quantity);
+                } else {
+                    if (tokensCount > 0 && tokensCount <= 4) {
+                        // Remove dot tokens inside
+                        _getTokensElements()
+                            .forEach(function(token) {
+                                token.remove();
+                            });
+                    } else if (tokensCount >= 5) {
+                        // Remove text token inside
+                        _getTokensElements().remove();
+                    }
+                    _drawNumberToken(quantity);
+                }
+            }
+
+            /**
+             * Create the SVG dot elements for the tokens if its less or equal than 4
+             **/
+            function _drawDotTokens(quantity) {
+                var tokensCount = _countTokenElements();
+
+                // Draw more dots if needed
+                for (var i = quantity - tokensCount; i > 0; i--) {
+                    place.parent()
+                        .circle(5)
+                        .addClass('token-dot')
+                        .fill('#000')
+                        .cx(place.cx())
+                        .cy(place.cy());
+                }
+
+                // Filter the tokens elements only
+                var tokenElements = place.parent()
+                    .children().filter(function(child, i) {
+                        return i && child.type == 'circle';
+                    });
+
+                // Adjust token element position
+                tokenElements.forEach(function(token, i) {
+                    var position = _getTokenPosition(token, place, i, quantity);
+                    token
+                        .animate(100)
+                        .cx(position.cx)
+                        .cy(position.cy);
+                });
+            }
+
+            /**
+             * Create the SVG's elements for the number of tokens if its more than 4
+             **/
+            function _drawNumberToken(number) {
                 place.parent()
-                    .circle(10)
-                    .addClass('token-dot')
-                    .fill('#000')
+                    .text(number + "")
+                    .attr({
+                        'font-weight': 600,
+                        'font-size': 20
+                    }).
+                style('user-select', 'none')
+                    .addClass('token-number')
                     .cx(place.cx())
                     .cy(place.cy());
             }
 
-            // Filter the tokens elements only
-            var tokens = place.parent()
-                .children().filter(function(child, i) {
-                    return i && child.type == 'circle';
-                });
-
-            // Adjust token element position
-            tokens.forEach(function(token, i) {
-                var position = _getTokenPosition(token, place, i, qty);
-                token
-                    .animate(100)
-                    .cx(position.cx)
-                    .cy(position.cy);
-            });
-        }
-
-        function _drawNumberToken(place, number) {
-            place.parent()
-                .text(number+"")
-                .attr({
-                    'font-weight': 600,
-                    'font-size': 20
-                })
-                .style('user-select', 'none')
-                .addClass('token-number')
-                .cx(place.cx())
-                .cy(place.cy());
-        }
-
-        function _countTokens(place) {
-            var tokens = _getTokens(place);
-            var tokensCount;
-            if(tokens) {
-                tokensCount = (tokens.length > 0)? tokens.length : 0
-                return tokensCount || tokens.text() - 0;
+            /**
+             * Count the quantity of tokens elements inside the place
+             **/
+            function _countTokenElements() {
+                var tokenElements = _getTokensElements();
+                var tokensCount;
+                if (tokenElements) {
+                    tokensCount = (tokenElements.length > 0) ? tokenElements.length : 0
+                    return tokensCount || tokenElements.text() - 0;
+                } else
+                    return 0;
             }
-            else
-                return 0;
-        }
 
-        function _getTokens(place) {
-            var tokensDot = place.parent().select('.token-dot').members;
-            var tokentext = place.parent().select('.token-number').members['0'];
+            /**
+             * Return the tokens elements
+             **/
+            function _getTokensElements() {
+                var tokensDot = place.parent().select('.token-dot').members;
+                var tokentext = place.parent().select('.token-number').members['0'];
 
-            if (tokensDot.length > 0) {
-                return tokensDot;
-            } else {
-                return tokentext;
+                if (tokensDot.length > 0) {
+                    return tokensDot;
+                } else {
+                    return tokentext;
+                }
             }
-        }
 
-        function _getTokenPosition(token, place, index, total) {
-            var position;
-            var margin = 2;
+            /**
+             * Compute the positions to organize the dot tokens inside the element
+             **/
+            function _getTokenPosition(token, place, index, total) {
+                var position;
+                var margin = 2;
 
-            switch (total) {
-                case 1:
-                    position =  {
-                        cx: place.cx(),
-                        cy: place.cy()
-                    };
-                    break;
-                case 2:
-                    if (index == 0) {
-                        position =  {
-                            cx: place.cx() - (token.width()/2+margin),
-                            cy: place.cy()
-                        };
-                    } else if (index == 1) {
-                        position =  {
-                            cx: place.cx() + (token.width()/2+margin),
-                            cy: place.cy()
-                        };
-                    }
-                    break;
-                case 3:
-                    if (index == 0) {
-                        position =  {
+                switch (total) {
+                    case 1:
+                        position = {
                             cx: place.cx(),
-                            cy: place.cy() - (token.height()/2+margin)
+                            cy: place.cy()
                         };
-                    } else if (index == 1) {
-                        position =  {
-                            cx: place.cx() - (token.width()/2+margin),
-                            cy: place.cy() + (token.height()/2+margin)
-                        };
-                    } else if (index == 2) {
-                        position =  {
-                            cx: place.cx() + (token.width()/2+margin),
-                            cy: place.cy() + (token.height()/2+margin)
-                        };
-                    }
-                    break;
-                case 4:
-                    if (index == 0) {
-                        position =  {
-                            cx: place.cx() - (token.width()/2+margin),
-                            cy: place.cy() - (token.height()/2+margin)
-                        };
-                    } else if (index == 1) {
-                        position =  {
-                            cx: place.cx() + (token.width()/2+margin),
-                            cy: place.cy() - (token.height()/2+margin)
-                        };
-                    } else if (index == 2) {
-                        position =  {
-                            cx: place.cx() - (token.width()/2+margin),
-                            cy: place.cy() + (token.height()/2+margin)
-                        };
-                    } else if (index == 3) {
-                        position =  {
-                            cx: place.cx() + (token.width()/2+margin),
-                            cy: place.cy() + (token.height()/2+margin)
-                        };
-                    }
-                    break;
-            }
+                        break;
+                    case 2:
+                        if (index == 0) {
+                            position = {
+                                cx: place.cx() - (token.width() / 2 + margin),
+                                cy: place.cy()
+                            };
+                        } else if (index == 1) {
+                            position = {
+                                cx: place.cx() + (token.width() / 2 + margin),
+                                cy: place.cy()
+                            };
+                        }
+                        break;
+                    case 3:
+                        if (index == 0) {
+                            position = {
+                                cx: place.cx(),
+                                cy: place.cy() - (token.height() / 2 + margin)
+                            };
+                        } else if (index == 1) {
+                            position = {
+                                cx: place.cx() - (token.width() / 2 + margin),
+                                cy: place.cy() + (token.height() / 2 + margin)
+                            };
+                        } else if (index == 2) {
+                            position = {
+                                cx: place.cx() + (token.width() / 2 + margin),
+                                cy: place.cy() + (token.height() / 2 + margin)
+                            };
+                        }
+                        break;
+                    case 4:
+                        if (index == 0) {
+                            position = {
+                                cx: place.cx() - (token.width() / 2 + margin),
+                                cy: place.cy() - (token.height() / 2 + margin)
+                            };
+                        } else if (index == 1) {
+                            position = {
+                                cx: place.cx() + (token.width() / 2 + margin),
+                                cy: place.cy() - (token.height() / 2 + margin)
+                            };
+                        } else if (index == 2) {
+                            position = {
+                                cx: place.cx() - (token.width() / 2 + margin),
+                                cy: place.cy() + (token.height() / 2 + margin)
+                            };
+                        } else if (index == 3) {
+                            position = {
+                                cx: place.cx() + (token.width() / 2 + margin),
+                                cy: place.cy() + (token.height() / 2 + margin)
+                            };
+                        }
+                        break;
+                }
 
-            return position;
+                return position;
+            }
         }
     }
 
-    angular.module('petriNet.common').factory('placeFactory', places);
+    angular.module('petriNet.common').factory('placeFactory', placeFactory);
 })();
